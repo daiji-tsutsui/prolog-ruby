b4a5839332bcf0f91783cc6aaeaffab93c630ed2
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2026-02-23 02:13:04 +0900

    テストケース追加

---
+++ spec/sample/hoge_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+RSpec.describe Prolog do
+  describe 'single unification with facts' do
+    before do
+      @hoge = Prolog::Predicate.new('hoge', [
+        { key: 1, goals: [{ predicate: true, args: [] }] },
+        { key: 3, goals: [{ predicate: true, args: [] }] },
+      ])
+      $stdout = StringIO.new
+    end
+
+    after do
+      $stdin = STDIN
+      $stdout = STDOUT
+    end
+
+    context 'when input y for confirmation' do
+      before do
+        $stdin = StringIO.new('y')
+      end
+
+      it 'returns OK for the fact hoge(1)' do
+        expect(@hoge.ok?(1)).to be_truthy
+      end
+
+      it 'returns OK for the fact hoge(3)' do
+        expect(@hoge.ok?(3)).to be_truthy
+      end
+
+      it 'returns NOT OK for a non-exist fact' do
+        expect(@hoge.ok?(2)).to be_falsy
+      end
+
+      it 'truncates backtracking' do
+        @hoge.ok?(1)
+        expect($stdout.string).to include '[TRUE] hoge?(( 1 ))'
+        expect($stdout.string).not_to include '[UNIF] 1 <--> 3'
+        expect($stdout.string).not_to include '[FALSE] hoge?(1)'
+      end
+    end
+
+    context 'when input N for confirmation' do
+      before do
+        $stdin = StringIO.new('N')
+      end
+
+      it 'returns always NOT OK case 1' do
+        expect(@hoge.ok?(1)).to be_falsy
+      end
+
+      it 'returns always NOT OK case 2' do
+        expect(@hoge.ok?(2)).to be_falsy
+      end
+
+      it 'returns always NOT OK case 3' do
+        expect(@hoge.ok?(3)).to be_falsy
+      end
+
+      it 'backtracks' do
+        @hoge.ok?(1)
+        expect($stdout.string).not_to include '[TRUE]'
+        expect($stdout.string).to include '[UNIF] 1 <--> 3'
+      end
+    end
+  end
+end
