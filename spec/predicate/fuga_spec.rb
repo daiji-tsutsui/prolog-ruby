e47cb7e48c301a2f834cbac34b9b56ef88462379
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2026-02-27 19:11:19 +0900

    fugaのUT追加

---
+++ spec/predicate/fuga_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+RSpec.describe Prolog::Predicate do
+  describe '[fuga] predicate defined with a variable' do
+    before do
+      _hoge = Prolog::Predicate.new(name: 'hoge') do |hoge, e|
+        hoge[1] = e.true
+        hoge[3] = e.true
+      end
+
+      @X1 = Prolog::Expression::Variable.new
+      @fuga = Prolog::Predicate.new(name: 'fuga') do |fuga, e|
+        fuga[1] = e.hoge(1)
+        fuga[@X1] = e.hoge(@X1) & e.hoge(@X1 - 2)
+      end
+
+      $stdin = StringIO.new('N')
+      $stdout = StringIO.new
+    end
+
+    after do
+      $stdin = STDIN
+      $stdout = STDOUT
+    end
+
+    describe 'fuga(1)' do
+      subject { @fuga.ok?(1) }
+      it 'returns OK for the rule fuga(1) -> hoge(1)' do
+        $stdin = StringIO.new('y')
+        is_expected.to be_truthy
+        expect($stdout.string).to include "[UNIF] 1 <--> 1\n--> finish? [y/N]"
+        expect($stdout.string).to include '[TRUE] hoge?(( 1 ))'
+        expect($stdout.string).to include '[TRUE] fuga?(( 1 ))'
+      end
+    end
+
+    describe 'fuga(3)' do
+      subject { @fuga.ok?(3) }
+      it 'returns OK for the rule fuga(X) -> hoge(X) && hoge(X - 2)' do
+        $stdin = StringIO.new('y')
+        is_expected.to be_truthy
+        expect($stdout.string).to match %r{\[UNIF\] 3 <--> .*Prolog::Variable.*\n--> finish?}
+        expect($stdout.string).to include '[TRUE] hoge?(( 3 ))'
+        expect($stdout.string).to include '[TRUE] hoge?(( 1 ))'
+        expect($stdout.string).to include '[TRUE] fuga?(( 3 ))'
+      end
+    end
+
+    describe 'fuga(2)' do
+      subject { @fuga.ok?(2) }
+      it 'returns NOT OK for all the rules' do
+        $stdin = StringIO.new('y')
+        is_expected.to be_falsy
+        expect($stdout.string).to include '[FALSE] fuga?(2) <--'
+      end
+    end
+  end
+end
